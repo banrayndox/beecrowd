9181513c582199e965944f5d033a003ad793fd48
banrayndox <banrayndox@gmail.com>
2025-07-20 11:46:41 +0600

    Create sol_1081.c

---
+++ sol_1081.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+
+int L;
+char T;
+scanf("%d",&L);
+getchar();
+scanf("%c",&T);
+float sum=0.0;
+float M[12][12];
+for(int i=0; i<12; i++){
+    for(int j=0; j<12; j++){
+        scanf("%f",&M[i][j]);
+    }
+}
+
+for(int i=0; i<12; i++){
+    sum+=M[L][i];
+}
+if(T=='S'){
+    printf("%.1f\n", sum);
+} else if(T=='M'){
+    printf("%.1f\n",sum/12.0);
+}
+
+ 
+    return 0;
+}
