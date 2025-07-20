0e8f8f504fbffb399101d197351c07a29f3f2671
banrayndox <banrayndox@gmail.com>
2025-07-20 14:17:03 +0600

    Create sol_1185.c

---
+++ sol_1185.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ #include<string.h>
+int main() {
+char O;
+scanf("%c",&O);
+getchar();
+float M[12][12]; 
+int sDiagonalUp=0;
+float sum=0.0;
+for(int i=0; i<12; i++){
+for(int j=0; j<12; j++){
+    scanf("%f",&M[i][j]);    
+    if(i+j<12-1){
+    sDiagonalUp++;
+    sum+=M[i][j];
+}
+}
+}
+if(O=='S'){
+    printf("%.1f\n", sum);
+}else if(O=='M'){
+    printf("%.1f\n",sum/sDiagonalUp);
+}
+
+ 
+ 
+ 
+    return 0;
+}
