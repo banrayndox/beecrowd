6ddb22425e4e589e8c7632243e982494b451f6db
banrayndox <banrayndox@gmail.com>
2025-07-20 15:02:19 +0600

    Create sol_1188.c

---
+++ sol_1188.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main() {
+#include <stdio.h>
+#include<string.h>
+#include<math.h>
+int main() {
+
+char O;
+scanf("%c", &O);
+getchar();
+double M[12][12];
+double sum=0.0;
+int count=0;
+for(int i=0; i<12; i++){
+    for(int j=0; j<12; j++){
+    scanf("%lf",&M[i][j]);
+    if(i>j && (i+j>12-1)){
+       count++;
+       sum+=M[i][j];
+    }
+    }
+    
+}
+
+ double result = (O == 'S') ? sum : sum / count;
+    result = result + 0.0001;
+    
+    printf("%.1lf\n", result); 
+    return 0;
+}
+    return 0;
+}
