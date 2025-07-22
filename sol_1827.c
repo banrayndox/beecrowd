bac9896e60d9642dc605695b5abfff2f9dccc0df
banrayndox <banrayndox@gmail.com>
2025-07-22 15:18:21 +0600

    Update sol_1827.c

--- sol_1827.c
+++ sol_1827.c
@@ -1,27 +1,28 @@
-// not yet done
 #include <stdio.h>
 
 int main() {
-int N;
-int i,j;
+int N; // size of matrix
+int i,j; // initializing loop variable
 while(scanf("%d", &N) !=EOF){
-int obs = N/3;    
-for(i=0; i<N; i++){
-    for(j=0; j<N; j++){
-    int x=0;
-    if(i==j) x=2;
-    if(i+j==N-1) x=3;
-    if(i==j && i+j==N-1) x=4;
-    printf("%d",x);
-    if(j==N-1){
-        printf("\n");
-    }else{
-        printf(" ");
-    }
-    
+int start = N/3;  //  start inner square
+int end = N-start; // end of inner square
+for(i=0; i<N; i++){ // main loop
+
+    for(j=0; j<N; j++){ // secondery loop
+    int x=0; // default or outer
+    if(i==j) x=2; //first diagonal 
+    if(i+j==N-1) x=3; //secondery diagonal
+        if (i >= start && i < end && j >= start && j < end) x = 1;  //inner
+    if(i==j && i+j==N-1) x=4; //center point
+
+
+    printf("%d",x); // print
+
+    if(j==N-1)  printf("\n"); // new line
+
 }    
 }    
-    printf("\n");
+    printf("\n"); //endline
     
 }
     return 0;
